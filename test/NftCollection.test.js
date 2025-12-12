1ba3a76d444ca9ac46c252ad5ac12fb444c2799c
Maharshi Denuvakonda <maharshi.dv.kld@gmail.com>
2025-12-12 20:06:24 +0530

    Updated NftCollection.test.js

--- test/NftCollection.test.js
+++ test/NftCollection.test.js
@@ -5,9 +5,6 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("NftCollection", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset the Hardhat Network to that snapshot in every test.
   async function deployNftFixture() {
     const MAX_SUPPLY = 20;
 
